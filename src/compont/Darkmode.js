25ec1af4a6bfbe7c37461a3e993c5ce081bb5b29
katyng1130 <nhumai.ktqd@gmail.com>
2023-03-16 19:07:46 +0800

    3

--- src/compont/Darkmode.js
+++ src/compont/Darkmode.js
@@ -6,9 +6,13 @@ function Darkmode() {
  
   const [mode, setMode] = useState("light")
   useEffect(() => {
+    const mode1= localStorage.getItem("theme") || "light"
+    setMode(mode1)
+
+  }, [ ]);
+  useEffect(() => {
     if (mode ==='dark') {
       document.body.classList.add("dark");
-      
     } else {
       document.body.classList.remove("dark");
     }
@@ -17,12 +21,12 @@ function Darkmode() {
 
     <div>
       <div>
-      <button type="button" onClick={() => {setMode("light")}}>
+      <button type="button" onClick={() => {setMode("light"); localStorage.setItem("theme", "light")}}>
         ☀ lightttttt 
       </button>
       <div> Hiiiii</div>
       
-      <button type="button" onClick={() => {setMode("dark")}}>
+      <button type="button" onClick={() => {setMode("dark"); localStorage.setItem("theme", "dark")}}>
         ☾ darkkkkk
       </button>
     </div>
