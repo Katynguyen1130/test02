948a035c3df6b8ad470d7fb77ba7d2429c68b476
katyng1130 <nhumai.ktqd@gmail.com>
2023-03-17 23:41:25 +0800

    fix name

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import Darkmode from "./../compont/Darkmode";
 import "./style.css";
-import Slider from "../compont/slider";
+import Slider from "../compont/Slider";
 
 const IndexPage = () => {
   return (
